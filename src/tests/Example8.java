d86d3c2479bfeada796186cd26731e2f6a4d13ca
ilirm <ilirm@ilirm-PC>
2018-07-12 16:28:14 -0500

    example 8 added

---
+++ src/tests/Example8.java
@@ -0,0 +1,23 @@
+package tests;
+
+import static math.Complex.* ;
+import math.Complex;
+
+/**
+ * This is just a demonstration of operator overloading for complex numbers
+ * Use "import static" for complex constants
+ * @author meisam
+ *
+ */
+
+public class Example8 {
+	public static void main(String[] args) {
+		// first creating a complex number
+		Complex u = 1+2*j ;
+		// then printing it to the consol
+		System.out.println("u = " + u);
+		// now performing mathematical tasks
+		Complex w = u*u -1 ;
+		System.out.println("w = " + w);
+	}
+}
